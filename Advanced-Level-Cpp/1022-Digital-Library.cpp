23c576a866786084af9a26418e498b0da32d58e8
knothhe <guanglaihe@gmail.com>
2019-09-04 19:39:56 +0800

    :art:

--- Advanced-Level-Cpp/1022-Digital-Library.cpp
+++ Advanced-Level-Cpp/1022-Digital-Library.cpp
@@ -1,60 +1,54 @@
-/*
- * reference:
- *  https://www.liuchuo.net/archives/2295
- * because I don't konw the correct way to read input.
- */
 #include <iostream>
 #include <sstream>
-#include <string>
 #include <map>
 #include <set>
 using namespace std;
-int N, M;
-map<string, set<int>> title, author, key, pub, year;
-void query(map<string, set<int>> &m, string &str) {
-    if (m.find(str) != m.end()) {
-        for (const auto &n : m[str]) {
-            printf("%07d\n", n);
+void query(map<string, set<int>> &m, string &q) {
+    if (m.find(q) != m.end()) {
+        for (int item : m[q]) {
+            printf("%07d\n", item);
         }
     } else {
-        cout << "Not Found\n";
+        printf("Not Found\n");
     }
 }
-int main()
-{
-    int id;
-    string t_title, t_author, t_keys, t_key, t_pub, t_year;
-    scanf("%d", &N);
-    for (int i = 0; i < N; i++) {
+int main() {
+    int n, m;
+    map<string, set<int>> title, author, key, publisher, year;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int id;
+        string str, keyword;
         scanf("%d\n", &id);
-        getline(cin, t_title);
-        title[t_title].insert(id);
-        getline(cin, t_author);
-        author[t_author].insert(id);
-        getline(cin, t_keys);
-        istringstream iss(t_keys);
-        while (iss >> t_key) {
-            key[t_key].insert(id);
-        }
-        getline(cin, t_pub);
-        pub[t_pub].insert(id);
-        getline(cin, t_year);
-        year[t_year].insert(id);
-    }
-    scanf("%d", &M);
-    for (int i = 0; i < M; i++) {
-        int q;
-        string str;
-        scanf("%d: ", &q);
         getline(cin, str);
-        cout << q << ": " << str << endl;
-        switch (q) {
+        title[str].insert(id);
+        getline(cin, str);
+        author[str].insert(id);
+        getline(cin, str);
+        istringstream iss(str);
+        while (iss >> keyword) {
+            key[keyword].insert(id);
+        }
+        getline(cin, str);
+        publisher[str].insert(id);
+        getline(cin, str);
+        year[str].insert(id);
+    }
+    cin >> m;
+    for (int i = 0; i < m; i++) {
+        int type;
+        string str;
+        scanf("%d: ", &type);
+        getline(cin, str);
+        printf("%d: %s\n", type, str.c_str());
+        switch (type) {
             case 1 : query(title, str); break;
             case 2 : query(author, str); break;
             case 3 : query(key, str); break;
-            case 4 : query(pub, str); break;
+            case 4 : query(publisher, str); break;
             case 5 : query(year, str); break;
         }
     }
     return 0;
 }
+
--- Advanced-Level-Cpp/1031-Hello-World-for-U.cpp
+++ Advanced-Level-Cpp/1031-Hello-World-for-U.cpp
@@ -5,8 +5,8 @@ string u;
 int main() {
     cin >> u;
     int N = u.size() + 2;
-    int n1 = N / 3, n2 = N / 3 + N % 3;
-    n1 = (N - n2 + 2) / 2;
+    int n2 = N / 3 + N % 3;
+    int n1 = (N - n2 + 2) / 2;
     int li = 0, ri = u.size() - 1;
     while (li < ri && ri - li + 1 != n2) {
         cout << u[li] << string(n2 - 2, ' ') << u[ri] << '\n';
